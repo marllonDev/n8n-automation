3c9fbba2d6fa262f65c9693283bca3c202f03315
Marllon Zucolotto <marlonzucolotto@gmail.com>
2025-08-01 15:16:28 -0300

    Update s3_setup.sql

--- n8n-compose/local-files/s3_setup.sql
+++ n8n-compose/local-files/s3_setup.sql
@@ -3,5 +3,5 @@ INSTALL s3;
 LOAD s3;
 SET s3_endpoint = 'http://minio:9000';
 SET s3_url_style = 'path';
-SET s3_access_key_id = 'adm'; -- Seu MINIO_ROOT_USER
-SET s3_secret_access_key = 'meuMinIo'; -- Seu MINIO_ROOT_PASSWORD
+SET s3_access_key_id = ''; -- Seu MINIO_ROOT_USER
+SET s3_secret_access_key = ''; -- Seu MINIO_ROOT_PASSWORD
